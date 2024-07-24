23314f2ae07f37c5d6b7ab63928d9d87f259b8cf
divyangshu-19 <divyangshupandey@gmail.com>
2024-07-24 18:42:13 +0530

    nav css isolated

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,48 +1,48 @@
 
 
-import React, { useState, useEffect } from "react";
+  import React, { useState, useEffect } from "react";
+  import "./nav.css";
 
+  function Nav(){
+    
+      const [isScrolled, setIsScrolled] = useState(false);
 
-function Nav(){
-   
-    const [isScrolled, setIsScrolled] = useState(false);
+      useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 1) {
+            setIsScrolled(true);
+          } else {
+            setIsScrolled(false);
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+    
+    
+    
+    
+      return(
+          
+          <div className={`navBar ${isScrolled ? "scrolled" : ""}`}>
+              <nav className={`navElements ${isScrolled ? "scrolled" : ""}`}>
+                  <div id="logo">
+                    <a href="#">Vantra<span>.</span></a>  
+                  </div>
+                  <div className="navItems">
+                      <a href="#">Services</a>
+                      <a href="#">Work</a>
+                      <a href="#">About</a>
+                  </div>
+                  <div className="start">
+                      <button>Lets Start</button>
+                  </div>
+              </nav>
+          </div>
+      )
+  }
 
-    useEffect(() => {
-      const handleScroll = () => {
-        if (window.scrollY > 1) {
-          setIsScrolled(true);
-        } else {
-          setIsScrolled(false);
-        }
-      };
-  
-      window.addEventListener("scroll", handleScroll);
-      return () => {
-        window.removeEventListener("scroll", handleScroll);
-      };
-    }, []);
-   
-   
-   
-   
-    return(
-        
-        <div className={`navBar ${isScrolled ? "scrolled" : ""}`}>
-            <nav className={`navElements ${isScrolled ? "scrolled" : ""}`}>
-                <div id="logo">
-                  <a href="#">Vantra<span>.</span></a>  
-                </div>
-                <div className="navItems">
-                    <a href="#">Services</a>
-                    <a href="#">Work</a>
-                    <a href="#">About</a>
-                </div>
-                <div className="start">
-                    <button>Lets Start</button>
-                </div>
-            </nav>
-        </div>
-    )
-}
-
-export default Nav
+  export default Nav
